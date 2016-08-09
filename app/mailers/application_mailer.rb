c9ed2329e3a52c5a3b7baf428b84d907a06318a6
Raymond427 <rferrel427@gmail.com>
2016-08-08 21:58:09 -0700

    Created mail views

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def welcome_email(member)
+    @member = member
+    @url  = 'http://example.com/login'
+    mail(to: @member.email, subject: 'Welcome to My Awesome Site')
+  end
 end
