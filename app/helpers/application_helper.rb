799a43eaa3a79342ef716906be127feef2feb8e1
Raymond427 <rferrel427@gmail.com>
2016-09-29 12:45:02 -0400

    fixed login form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def logo_image
+    attachments.inline['logo.png'] = File.read('app/assets/images/logo.png')
+    image_tag attachments['logo.png'].url
+  end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'noreply-aim@gmail.com'
   layout 'mailer'
+  def email_image_tag(image, **options)
+    attachments[image] = File.read(Rails.root.join("app/assets/images/#{image}"))
+    image_tag attachments[image].url, **options
+  end
 
   def welcome_email(member)
-    #attachments.inline['image.jpg'] = File.read('/path/to/image.jpg')
     @member = member
     @school_name = @member.chapter.school
     mail(to: @member.email, subject: 'Welcome to Advancing Innovative Minds!')
